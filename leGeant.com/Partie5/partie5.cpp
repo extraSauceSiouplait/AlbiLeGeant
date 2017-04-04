0823a99cb9751967f90a537f862a20445cb731f3
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2017-04-04 16:58:02 -0400

    code fonctionnel partie 5 (photoresistances).

--- leGeant.com/Partie5/partie5.cpp
+++ leGeant.com/Partie5/partie5.cpp
@@ -8,6 +8,8 @@
 
 #define DROIT 'd' 
 #define GAUCHE 'g'
+#define ROUGE 0x01
+#define VERT 0x02
 
 
 //Sensor gauche @ A0
@@ -16,48 +18,52 @@
 uint16_t valeurCan16bitDroit;
 uint16_t valeurCan16bitGauche;
 char cote;
+bool avantINT = true;
+
 
 ISR(INT0_vect);
 
 int main()
 {   
-    DDRC = 0xff;
-    DDRD = 0xff;
+    DDRC = 0xff;    //portC en ecriture pour la led
     DDRA = 0x00;    //portA en lecture
-    PORTC = 0x02;
     can convertisseur;
     initialisationINT0(1,1);   //rising edge
     
     /* CODE POUR ARRETE DE BOUGER */ 
     
     for(;;){
-        
         valeurCan16bitDroit = convertisseur.lecture(PINA1);   //lecture pin1 portA
         valeurCan16bitDroit >>= 2;
         valeurCan16bitGauche = convertisseur.lecture(PINA0);    //lecture pin0 portA
         valeurCan16bitGauche >>= 2;
         
-        if(valeurCan16bitDroit > 230) 
+        if(valeurCan16bitDroit > 230 && avantINT) {
             cote = DROIT;
+            _delay_ms(200);
+        }
     
-        else if(valeurCan16bitGauche > 230) 
+        else if(valeurCan16bitGauche > 230 && avantINT) {
             cote = GAUCHE;
-        
-       
+            _delay_ms(200);
+        }
     }
 }
 
 ISR(INT0_vect){
+    avantINT = false;
     switch(cote){
         case DROIT:
             //tourner a droite
-            PORTC = 0x01;
+            PORTC = ROUGE;
+            break;
         case GAUCHE:
             //tourner a gauche
-            PORTC = 0x02;
+            PORTC = VERT;
+            break;
     }
-    
     EIMSK &= ~(1 << INT0); //desactive external interrupt 0.
+    /* ETAT SUIVANT */
             
 }
 
