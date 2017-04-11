4da19a16ffaeab2c42e9c59c89a509abde5a257e
Samuel Meilleur <samuel.meilleur@polymtl.ca>
2017-04-11 15:21:54 -0400

    Implementation de la classe Moteurs dans la classe capteurs.

--- leGeant.com/librairie/capteurs.cpp
+++ leGeant.com/librairie/capteurs.cpp
@@ -1,5 +1,4 @@
-#include "capteurs.h"
-#include "librairieFonctions.h"
+#include "header.h"
 
 //Constructeur par défaut
 //Initialise la lecture des capteurs sur le port A
@@ -7,7 +6,6 @@
 bool Capteurs::getSensor(uint8_t indice) {
     return sensors_[indice];
 }
-
 
 Capteurs::Capteurs() {
     DDRA = 0x00; //Port A en entrée pour permettre lecture des capteurs
@@ -17,45 +15,36 @@ Capteurs::Capteurs() {
 
 void Capteurs::lecture() {
     for (int i = 0; i < 5; i++)
-        sensors_[i] = PINA & (1 << (i+2)); //capteurs sur PINA2 a 7
+        sensors_[i] = (PINA >> (i + 2)) & 0x01;     //lit chaque capteur et store l'etat dans un booleen.   (capteurs sur A2 a A7)
 }
 
 void  Capteurs:: lineTracking() {
-	ecrire0('D',2);
-	ecrire0('D',3);
-
-    if (sensors_[2] || (sensors_[1] && sensors_[3]))			//seulement le sensor du millieu = 1
-    {
+	Moteurs::avancer();
+    if (sensors_[2] || (sensors_[1] && sensors_[3])){       //seulement le sensor du millieu = 1
         // Le robot doit aller tout droit.
         ajustementPwmMoteurs(60,60);
     }
-
-    else if ((sensors_[1]||sensors_[0]) && !(sensors_[3]))		//sensor de gauche et/ou du centre = 1
-    {
+    else if ((sensors_[1] || sensors_[0]) && !(sensors_[3])){    //sensor de gauche et/ou du centre = 1
         // Le robot doit tourner a gauche.
         ajustementPwmMoteurs(65, 45);
     }
-
-    else if ((sensors_[3]||sensors_[4]) && !(sensors_[1]))
-    {
+    else if ((sensors_[3] || sensors_[4]) && !(sensors_[1])){
         // Le robot doit tourner a droite.
         ajustementPwmMoteurs(45, 65);
     }
-
 }
 
 void Capteurs::tournerGauche()
 {
-	ecrire1('D', 2); //On fixe les directions de rotation des roues, afin qu'elles tournent en sens inverse l'une par rapport à l'autre. Ainsi, l'axe de rotation est approximativement le centre du robot.
-    ecrire0('D', 3);
+	Moteurs::tournerGauche();
     ajustementPwmMoteurs(100,100);
     _delay_ms(50);
 	ajustementPwmMoteurs(50, 50);	//Débuter rotation vers la gauche du robot
 }
+
 void Capteurs::tournerDroite()
 {
-	ecrire0('D',2);	//On fixe les directions de rotation des roues, afin qu'elles tournent en sens inverse l'une par rapport à l'autre. Ainsi, l'axe de rotation est approximativement le centre du robot.
-	ecrire1('D',3);
+	Moteurs::tournerDroit();
     ajustementPwmMoteurs(100,100);
     _delay_ms(50);
 	ajustementPwmMoteurs(50,50);	//Débuter roation vers la droite du robot
@@ -97,7 +86,6 @@ void Capteurs::intersectionGauche()
 {
 	ajustementPwmMoteurs(60,60);
 	_delay_ms(800);//MODIFIER VALEUR					//la ligne perpendiculaire au robot croise son axe de rotation
-
 
 	tournerGauche();			//debuter rotation
 	lecture();
